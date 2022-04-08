a5ada0442b6df41f79d3431dfcd2193ca52b38a1
SpOOk <spook.devv@gmail.com>
2022-04-08 15:06:44 -0400

    deleted the first blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView,
+)
 
 # url patterns
 urlpatterns = [
@@ -8,5 +14,5 @@ urlpatterns = [
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
-    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name = "post_delete")
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
 ]
