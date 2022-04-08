9b7998a62d08ad3f0d4b04be0953524ee6fd01fa
SpOOk <spook.devv@gmail.com>
2022-04-08 14:23:45 -0400

    added new urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 # url patterns
 urlpatterns = [
     # all entries start with post/, the primary key is represented by an integer
-    path("", BlogListView.as_view(), name = "home"),
-    path("post/<int:pk>/", BlogDetailView.as_view(), name = "post_detail")
+    path("", BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
 ]
